cdbb637993bef70a658a03834f9b6505df3261cd
Michael Melkumov <MMiknich@gmail.com>
2018-11-15 11:12:51 +0300

    Added hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+//
+// Created by mmiknich on 15.11.18.
+//
+
+#include <iostream>
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
+
---
+++ sqr.cpp
@@ -0,0 +1,15 @@
+//
+// Created by mmiknich on 15.11.18.
+//
+
+#include <iostream>
+
+int main (int argv, char *args)
+{
+    if (argv > 1)
+    {
+        int input = atoi
+        printf("The square of %d",)
+    }
+}
+
